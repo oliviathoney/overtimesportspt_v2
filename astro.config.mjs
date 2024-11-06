942f64e513fa38c45b70d016be055d6d8d525456
Olivia Thoney <oliviapthoney@gmail.com>
2024-11-06 16:58:13 -0700

    Add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless'
+import partytown from '@astrojs/partytown'
+
 
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),],
   output: "server",
   adapter: vercel(),
 });
