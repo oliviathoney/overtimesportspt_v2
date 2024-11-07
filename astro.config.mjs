60c2ca893dd3446b4ac1163c89c2a9d00d1d221b
Olivia Thoney <oliviapthoney@gmail.com>
2024-11-06 17:00:56 -0700

    Add Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless'
 import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
+
 
 
 
 export default defineConfig({
+  site: 'https://overtimesportspt.com',
   integrations: [
     react(),
     tailwind(),
+    sitemap(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
