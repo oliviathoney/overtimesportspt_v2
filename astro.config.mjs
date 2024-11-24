3581b16b9b081d63624886f9c4c1830d9beebf8c
Olivia Thoney <oliviapthoney@gmail.com>
2024-11-23 19:25:00 -0700

    Add inline to script elements

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,8 +16,8 @@ export default defineConfig({
     sitemap(),
     partytown({
       config: {
-        forward: ["dataLayer.push"],
-      },
+        forward: ["dataLayer.push"]
+      }
     }),],
   output: "server",
   adapter: vercel(),
